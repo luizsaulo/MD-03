ff64b7f5a621e230f57b5276e9ab2f63af75b0e3
Saulo <saulo@blockforce.in>
2022-10-19 16:41:13 -0300

    finished the header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { HeaderContainer, HeaderContent } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo-ignite.svg';
 
 export function Header() {
@@ -6,7 +6,7 @@ export function Header() {
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt='' />
-                <button>Nova transação</button>
+                <NewTransactionButton>Nova transação</NewTransactionButton>
             </HeaderContent>
         </HeaderContainer>
     )
